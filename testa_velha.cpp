ed8867d11f73864933acbf92bdd967d7e06bcb42
root <root@DESKTOP-EDE7B0D.>
2024-11-30 00:00:52 -0300

    (Add) Teste 1:  Verifica se o X venceu.

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -10,13 +10,13 @@
 #include "catch.hpp"
 
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Teste 1 : Verifica se o X venceu." ) {
+	
 	int teste1[3][3]= {   { 2, 0, 1 }, 
 	                      { 2, 0, 1 },
 						  { 0, 2, 1 }
 					  };
     REQUIRE( VerificaVelha(teste1) == 1 );
-
 
 } 			
  
